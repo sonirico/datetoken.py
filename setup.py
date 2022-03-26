ce85d9172b4d7eacc26b90615250f35a610c25f5
Marquitos <sonirico@users.noreply.github.com>
2022-03-26 12:16:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=3.6',
     install_requires=[
         'pytz>=2018.04,<2021.4',
         'python-dateutil>=2.7.3,<2.8.3'
